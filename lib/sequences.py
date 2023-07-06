50c0227984f979fb5f4f844c6a266bef606f537b
Ngoc Nguyen <nnpk0410@gmail.com>
2023-07-06 13:12:16 -0500

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
+
 def print_fibonacci(length):
-    pass
+    fib_list = [] # Create an empty list to store fibonacci numbers
+    a, b = 0, 1 # Initialize the first two numbers of the sequence
+
+    for _ in range(length):
+        fib_list.append(a)  # Add the current Fibonacci number to the list
+        a, b = b, a + b  # Update the Fibonacci numbers for the next iteration
+
+    print(fib_list)
+
